4202a94adbbaa3ac998057ecfee0a0e268dfbb2f
Firdosh-Khatri <firdous.khatri022@gmail.com>
2024-10-07 15:25:16 +0530

    Add files via upload

---
+++ Token_No_for _clinic.py
@@ -0,0 +1,8 @@
+n=1
+c=1
+print("Hello, welcome here")
+while(c==1):
+    print("Token no. ", n, "please come")
+    c=int(input("Tap 1 to continue or 0 to stop"))
+    n = n + 1
+print("This is the end")
